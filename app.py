350d18f974c4604c547886b640ef5b5e05fb9dcd
pravinpk9 <83226357+pravinpk9@users.noreply.github.com>
2022-03-03 15:31:24 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 import numpy as np
@@ -12,7 +12,7 @@ from pickle import load
 from flask import Flask, redirect, url_for, request, render_template, jsonify
 
 
-# In[9]:
+# In[2]:
 
 
 # Load saved models
@@ -25,7 +25,7 @@ scaler = load(open(SCALER_PATH, 'rb'))
 #print('Loading all saved models completed')
 
 
-# In[10]:
+# In[43]:
 
 
 # Define a flask app
@@ -33,15 +33,48 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def model_predict():
+    #return "1"
     val1 = request.args['values']
+    days = request.args['days']
+    
+    print(val1)
+    print(days)
+#     empty_array = np.array([])
+#     for i in val1.split(','):
+#         print(scaler.transform(np.array([float(i)])))
+#         j = scaler.transform(np.array([float(i)]))
+#         empty_array= np.append(empty_array, np.array([float(j)]))
+    
+#     pred = np.array([empty_array])
+#     pred = np.reshape(pred, (pred.shape[0],1, pred.shape[1]))
+#     res = model.predict(pred)
+#     print(res)
+#     print(res[0])
+#     for x in res[0]:
+#         print(x)
+        
+        
+    pred_result = []
     empty_array = np.array([])
     for i in val1.split(','):
-        empty_array= np.append(empty_array, np.array([float(i)]))
+        print(scaler.transform(np.array([[float(i)]])))
+        j = scaler.transform(np.array([[float(i)]]))
+        empty_array= np.append(empty_array, j)
+
+    for index in range(int(days)):
+        print(empty_array[-4:])
+        pred = np.array([empty_array[-4:]])
+        pred = np.reshape(pred, (pred.shape[0], 1, pred.shape[1]))
+
+        pred_values = model.predict(pred)
+        print(pred_values)
+        empty_array= np.append(empty_array, pred_values)
+        for e in pred_values[0]:
+            pred_result.append(e)
+        print('---------')
     
-    pred = np.array([empty_array])
-    pred = np.reshape(pred, (pred.shape[0],1, pred.shape[1]))
-    res = model.predict(pred)
-    return jsonify(prediction = str(scaler.inverse_transform(res)))
+    return str(pred_result)
+    return jsonify(prediction = str(scaler.inverse_transform([pred_result])))
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
